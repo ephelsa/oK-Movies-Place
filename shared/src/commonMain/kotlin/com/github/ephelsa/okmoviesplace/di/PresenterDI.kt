e7744d65f98e96ae9c6c9eaf2c6a7417023b70df
Leonardo Pérez Castilla <leonardo.perez@monstar-lab.com>
2022-01-11 20:58:16 -0500

    Changes in DI for shared and androidApp modules

---
+++ shared/src/commonMain/kotlin/com/github/ephelsa/okmoviesplace/di/PresenterDI.kt
@@ -0,0 +1,16 @@
+package com.github.ephelsa.okmoviesplace.di
+
+import com.github.ephelsa.okmoviesplace.di.TagsDI.Presenter
+import com.github.ephelsa.okmoviesplace.presenter.movies.MoviesUserActionManager
+import org.kodein.di.DI
+import org.kodein.di.bindProvider
+import org.kodein.di.instance
+
+object PresenterDI {
+
+    val viewManagerModule = DI.Module("Presenter/ViewManager") {
+        bindProvider(Presenter.Movies) {
+            MoviesUserActionManager(instance(TagsDI.Dispatcher.IO), instance(), instance(), instance())
+        }
+    }
+}
